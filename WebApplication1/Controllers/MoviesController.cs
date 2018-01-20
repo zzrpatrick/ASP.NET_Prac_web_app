3b7ad08a6f84410bb91ba0c0da9bc3a47807850c
zzrpatrick <zzrpatrick@hotmail.com>
2018-01-20 16:56:40 -0600

    little changed

--- WebApplication1/Controllers/MoviesController.cs
+++ WebApplication1/Controllers/MoviesController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Index()
+        public ActionResult Random()
         {
-            return View();
+            var movie = new Movie() { Name = "Shrek!" };
+                    
+            return View(movie);
         }
     }
 }
