edc625e620a0c5e8eb9e2e508bf2e99ec37235fd
zzrpatrick <zzrpatrick@hotmail.com>
2018-01-21 17:07:46 -0600

    Action Parameters finished

--- WebApplication1/Controllers/MoviesController.cs
+++ WebApplication1/Controllers/MoviesController.cs
@@ -20,5 +20,22 @@ namespace WebApplication1.Controllers
             //return new EmptyResult();
             return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });    
         }
+
+        public ActionResult Edit(int id )
+        {
+            return Content("movieId=" + id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("PageIndex={0} & sortBy={1}", pageIndex, sortBy));
+        }
     }
 }
