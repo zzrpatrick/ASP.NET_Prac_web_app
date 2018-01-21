b2f05ade382b3c5f6f02eb85b34d96212fcbd28d
zzrpatrick <zzrpatrick@hotmail.com>
2018-01-21 14:49:09 -0600

    Jan 21st keep working

--- WebApplication1/Controllers/MoviesController.cs
+++ WebApplication1/Controllers/MoviesController.cs
@@ -13,8 +13,12 @@ namespace WebApplication1.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-                    
-            return View(movie);
+
+            //return View(movie);
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });    
         }
     }
 }
